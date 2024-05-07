2f3f6b793d222ce25473bd33ed6aa9fed8f8129a
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-05-07 22:13:09 +0300

    Reverse Vowels of a String Problem Solved

--- LeetCode75/Reverse Vowels of a String.playground/Contents.swift
+++ LeetCode75/Reverse Vowels of a String.playground/Contents.swift
@@ -1,5 +1,24 @@
 class Solution {
-    func reverseVowels(_ s: String) -> String {}
+    func reverseVowels(_ s: String) -> String {
+        var s = Array(s)
+        var vowels = Set<Character>(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"])
+        var i = 0
+        var j = s.count - 1
+        while i < j {
+            while i < j && !vowels.contains(s[i]) {
+                i += 1
+            }
+            while i < j && !vowels.contains(s[j]) {
+                j -= 1
+            }
+            s.swapAt(i, j)
+            i += 1
+            j -= 1
+        }
+        return String(s)
+    }
 }
 
-let solution =
+let solution = Solution()
+let result = solution.reverseVowels("hello")
+let result2 = solution.reverseVowels("leetcode")
