19c9f83cbe696fd91d585476f1ba74862b786776
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-05-08 21:31:05 +0300

    Product of Array Except Self Problem Solved

--- LeetCode75/Product of Array Except Self.playground/Contents.swift
+++ LeetCode75/Product of Array Except Self.playground/Contents.swift
@@ -1,32 +1,20 @@
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        var copyNums = nums
-        var i = 0
-        var temp = 1
-        let count = nums.count
+        var res = Array(repeating: 1, count: nums.count)
 
-        copyNums = nums.map { num in
-            let currentIndex = nums.index(i, offsetBy: num)
-            print(currentIndex, i, num)
-            if currentIndex != i {
-                if num == 0 {
-                    return 0
-                }
-                temp *= num
-                i += 1
-            } else {
-                if num == 0 {
-                    return 0
-                }
-                i += 1
-            }
-            if num == 0 {
-                return 0
-            }
-            return temp
+        var pre = 1
+        for i in 0..<nums.count {
+            res[i] *= pre
+            pre *= nums[i]
         }
 
-        return copyNums.reversed()
+        var post = 1
+        for i in (0..<nums.count).reversed() {
+            res[i] *= post
+            post *= nums[i]
+        }
+
+        return res
     }
 }
 
@@ -34,9 +22,9 @@ class Solution {
 let solution = Solution()
 
 // Test 1
-// let result1 = solution.productExceptSelf([1, 2, 3, 4])
-// assert(result1 == [24, 12, 8, 6], "Test 1 failed")
+ let result1 = solution.productExceptSelf([1, 2, 3, 4])
+ assert(result1 == [24, 12, 8, 6], "Test 1 failed")
 
 // Test 2
 let result2 = solution.productExceptSelf([-1, 1, 0, -3, 3])
-// assert(result2 == [0, 0, 9, 0, 0], "Test 2 failed")
+ assert(result2 == [0, 0, 9, 0, 0], "Test 2 failed")
