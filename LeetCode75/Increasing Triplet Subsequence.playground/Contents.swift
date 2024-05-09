1f91e285278bc3a093ef6acecc32277f07c7ab6f
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-05-09 17:14:28 +0300

    Increasing Triplet Subsequence Problem Solved

--- LeetCode75/Increasing Triplet Subsequence.playground/Contents.swift
+++ LeetCode75/Increasing Triplet Subsequence.playground/Contents.swift
@@ -1,26 +1,19 @@
 class Solution {
     func increasingTriplet(_ nums: [Int]) -> Bool {
         guard nums.count >= 3 else { return false }
-
-        var i = 0
-        var j = 1
-        var k = 2
         
-        print(nums.count)
+        var i: Int = .max
+        var j: Int = .max
         
-        while k < nums.count {
-            if nums[i] < nums[j] && nums[j] < nums[k] && nums[i] < nums[k] {
+        for num in nums {
+            if num < i {
+                i = num
+            } else if num > i && num < j {
+                j = num
+            } else if num > j {
                 return true
             }
-            else if nums[i] < nums[j] && nums[j] < nums[k + 1] && nums[i] < nums[k + 1] && k + 1 != nums.count {
-                return true
-            }
-
-            i += 1
-            j += 1
-            k += 1
         }
-
         return false
     }
 }
@@ -31,4 +24,4 @@ let solution = Solution()
 solution.increasingTriplet([1, 2, 3, 4, 5])
 solution.increasingTriplet([5, 4, 3, 2, 1])
 solution.increasingTriplet([2, 1, 5, 0, 4, 6])
-solution.increasingTriplet([20,100,10,12,5,13])
+solution.increasingTriplet([20, 100, 10, 12, 5, 13])
